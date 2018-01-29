91be4b1249421b8f14f4a951d397cf79a5d7284b
Nam <nambn1996@gmail.com>
2018-01-30 00:31:14 +0700

    Init

--- week1/treesort_bug.c
+++ week1/treesort_bug.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 struct node {
-  int           value;
-  struct node*  left;
-  struct node*  right;
+	int           value;
+	struct node*  left;
+	struct node*  right;
 };
 
 void AddTree(struct node**, int);
@@ -15,52 +15,59 @@ void FreeTree(struct node*);
 /* Tree sort */
 int main(void)
 {
-  struct node* rootp = 0x1;
-  int i;
-  int array[15] = {50, 12, 18, 70, 41, 19, 91, 1, 7, 6, 81, 65, 55, 20, 0};
+	struct node* rootp = 0;
+	int i;
+	int array[15] = {50, 12, 18, 70, 41, 19, 91, 1, 7, 6, 81, 65, 55, 20, 0};
 
-  for (i = 0; i < 15; i++) {
-    AddTree(&rootp, array[i]);
-  }
+	for (i = 0; i < 15; i++) {
+		AddTree(&rootp, array[i]);
+	}
 
-  WalkTree(rootp);
-  printf("\n");
-
-  FreeTree(rootp);
-
-  return 0;
+	WalkTree(rootp);
+	printf("\n");
+	FreeTree(rootp);
+	return 0;
 }
 
 void AddTree(struct node** pp, int val)
 {
-  /* create new node if *p is null */
-  if (*pp == (struct node *)0) {
-    *pp = (struct node *)malloc(sizeof(struct node));
-    (*pp)->value = val;
-  }
-  else if ((*pp)->value > val) {
-    AddTree(&(*pp)->left, val);
-  }
-  else if ((*pp)->value < val) {
-    AddTree(&(*pp)->right, val);
-  }
-  /* (*pp)->value == val */
-  else {
-    /* do nothing */
-  }
+	/* create new node if *p is null */
+	if (*pp == NULL ) {
+		struct node *temp = (struct node *)malloc(sizeof(struct node));
+		temp->value = val;
+		temp->left = NULL;
+		temp->right = NULL;
+		*pp = temp;
+	}
+	else if ((*pp)->value > val) {
+		AddTree(&(*pp)->left, val);
+	}
+	else if ((*pp)->value < val) {
+		AddTree(&(*pp)->right, val);
+	}
+	/* (*pp)->value == val */
+	else {
+		/* do nothing */
+	}
 }
 
 void WalkTree(struct node* p)
 {
-  WalkTree(p->left);
-  printf("%d ", p->value);
-  WalkTree(p->right);
+	if (p == NULL){
+		return;
+	}
+	WalkTree(p->left);
+	printf("%d ", p->value);
+	WalkTree(p->right);
 }
 
 void FreeTree(struct node* p)
 {
-  FreeTree(p->left);
-  FreeTree(p->right);
-  free(p);
-  return;
+	if (p == NULL){
+		return;
+	}
+	FreeTree(p->left);
+	FreeTree(p->right);
+	free(p);
+	return;
 }
