abd66c1a57635ce0602f07fd1a46c488f010e85a
Nam <nambn1996@gmail.com>
2018-05-27 02:53:00 +0700

    Finish TCP connect

--- PowerSupply/client.c
+++ PowerSupply/client.c
@@ -1,12 +1,45 @@
-/*
-* @Author: Nam
-* @Date:   2018-05-26 15:22:11
-* @Last Modified by:   Nam
-* @Last Modified time: 2018-05-26 17:23:27
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define BUFF_SIZE 8192
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	if(argc != 3) {
+		fprintf(stderr,"Usage: %s <Server IP> <Echo Port>\n",argv[0]);
+		exit(1);
+	}
+
+	int client_sock;
+	char buff[BUFF_SIZE];
+	struct sockaddr_in server_addr;
+	int msg_len, bytes_sent, bytes_received;
+
+    // Step 1: Construct socket
+	client_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Step 2: Specify server address
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(atoi(argv[2]));
+	server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    // Step 3: Request to connect server
+	if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) < 0)
+	{
+		perror("accept() failed\n");
+		return 0;
+	}
+
+	// Step 4: Communicate with server
+	while(1) {
+		
+	}
+	
+    // Step 5: Close socket
+	close(client_sock);
 	return 0;
 }
--- PowerSupply/server.c
+++ PowerSupply/server.c
@@ -8,12 +8,56 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "multi-process.h"
-#include "pipe.h"
 
+#define BACKLOG 10 /* Number of allowed connections */
 #define BUFF_SIZE 1024
+int _server_port;
 
 void connectMng_handle() {
 	tprintf("SERVER forked process connectMng ------------ pid: %d.\n", getpid());
+
+	int listen_sock, conn_sock;
+	char recv_data[BUFF_SIZE];
+	int bytes_sent, bytes_received;
+	struct sockaddr_in server;
+	struct sockaddr_in client;
+	int sin_size;
+
+    //Step 1: Construct a TCP socket to listen connection request
+	if ((listen_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket() failed\n");
+		exit(1);
+	}
+
+    //Step 2: Bind address to socket
+	bzero(&server, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(_server_port);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(listen_sock, (struct sockaddr *)&server, sizeof(server)) == -1) {
+		perror("bind() failed\n");
+		exit(1);
+	}
+
+    //Step 3: Listen request from client
+	if (listen(listen_sock, BACKLOG) == -1) {
+		perror("listen() failed\n");
+		exit(1);
+	}
+
+	//Step 4: Communicate with client
+	while (1) {
+        //accept request
+		sin_size = sizeof(struct sockaddr_in);
+		if ((conn_sock = accept(listen_sock, (struct sockaddr *)&client, &sin_size)) == -1) {
+			perror("accept() failed\n");
+			exit(1);
+		}
+
+        printf("A device connected\n");
+
+        close(conn_sock);
+    }
 }
 
 void powerSupply_handle() {
@@ -38,44 +82,16 @@ int main(int argc, char const *argv[])
 		fprintf(stderr, "Usage:  %s <Server Port>\n", argv[0]);
 		exit(1);
 	}
-
-	int svPort = atoi(argv[1]);
-	int svSock;
-	struct sockaddr_in svAddr; 
-	struct sockaddr_in clAddr; 
-
-	// Step 1: Construct a UDP socket
-	if ((svSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-		perror("socket() failed\n");
-		exit(1);
-	} 
-
-	// Step 2: Bind address to socket
-	svAddr.sin_family = AF_INET;
-	svAddr.sin_port = htons(servPort);
-	svAddr.sin_addr.s_addr = INADDR_ANY;
-	bzero(&(svAddr.sin_zero), 8);
-
-	if (bind(svSock, (struct sockaddr *) &svAddr, sizeof(svAddr)) < 0) {
-		perror("bind() failed\n");
-		exit(1);
-	}
-
-	// Step 3: Communicate with clients
-	while(1) {
-		
-	} // endwhile
-
-
 	printf("SERVER start, PID is %d.\n", getpid());
+
+	_server_port = atoi(argv[1]);
 
 	///////////////////////////////////
 	// start child process in SERVER //
 	///////////////////////////////////
-	pid_t connectMng, powerSupply, elePowerCtrl, powSupplyInfoAccess, logWrite;
+	pid_t connectMng, elePowerCtrl, powSupplyInfoAccess, logWrite;
 
 	connectMng = createProcess(connectMng_handle);
-	powerSupply = createProcess(powerSupply_handle);
 	elePowerCtrl = createProcess(elePowerCtrl_handle);
 	powSupplyInfoAccess = createProcess(powSupplyInfoAccess_handle);
 	logWrite = createProcess(logWrite_handle);
@@ -86,7 +102,7 @@ int main(int argc, char const *argv[])
 	}
 	else if(!processError()) {
 		tprintf("Everything was gone without error.\n");
-    } // endif
+	}
 
-    return 0;
+	return 0;
 }
